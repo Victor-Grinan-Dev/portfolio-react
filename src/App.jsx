c625b4d1a1cf30fa6e1c353686bdea863f6cc67f
victor grinan <victorgrinan@gmail.com>
2025-10-07 11:44:17 +0300

    create main files

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  HashRouter,
+  Routes,
+  Route,
+  // BrowserRouter,
+} from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+// import { initializeCountries } from "./features/countries/countriesSlice";
+
+import Layout from "./layout/Layout";
+import HomePage from "./components/home/HomePage";
+
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // dispatch(initializeCountries());
+    // eslint-disable-next-line
+  }, [dispatch]);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
 
-export default App
+          {/* <Route path="galleria/pienet-tatuointi" element={<Smalls />} /> */}
+
+        </Route>
+      </Routes>
+    </HashRouter>
+
+
+  );
+};
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import ErrorBoundary from './ErrorBoundary.jsx'
+import { Provider } from 'react-redux';
+import store from './app/app';
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <ErrorBoundary>
+    <StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </StrictMode>
+  </ErrorBoundary>
+);
