b07a543f2cd5e89cc5eab14b2a4167b1e16ef25a
victor grinan <victorgrinan@gmail.com>
2026-08-17 11:15:56 +0300

    add about and contact

--- src/components/home/sections/about/About.jsx
+++ src/components/home/sections/about/About.jsx
@@ -2,50 +2,50 @@ import SectionWrapper from "../../../UIs/sectionWrapper/SectionWrapper";
 
 const About = () => {
   return (
-    <SectionWrapper>
-       <div id="about" className="about"> 
-                    <article id="aboutArticle" class="bgLevel1">
-                    
-            <h2 class="title"> About me </h2>
+    <SectionWrapper id="about" classNameName="about">
 
-            <p class="about">
+                    <article id="aboutArticle" className="bgLevel1">
+                    
+            <h2 className="title"> About me </h2>
+
+            <p className="about">
                 <br />
                 <br />
-                I am a computers <b class="keyword">enthusiast</b>.
+                I am a computers <b className="keyword">enthusiast</b>.
                 <br />
                 I started learning coding independently in 2019, and in June 2023 I graduated in Information and
-                communication technologies from <a class="about-link"
+                communication technologies from <a className="about-link"
                     href="https://en.bc.fi/qualifications/full-stack-web-developer-program/" target="blank">Helsinki
                     Business College (HBC)</a>, where I gained a solid understanding of programming, algorithms, and
                 software design principles.
                 <br />
                 My background is in pedagogy and team leading. This has taught me to see the value of
-                good <b class="keyword">group dynamics</b>. In my previous workplaces, I have quickly advanced to responsible
-                positions. I am particularly good at problem solving and <b class="keyword">customer service</b>. My hobbies have taught me <b class="keyword">patience</b>
-                and <b class="keyword">perseverance</b>. Now I'm studying to be <b class="keyword">IT-support</b> staff at <a class="about-link" href="https://www.taitotalo.fi/" target="blank">Taitotalo</a>.
+                good <b className="keyword">group dynamics</b>. In my previous workplaces, I have quickly advanced to responsible
+                positions. I am particularly good at problem solving and <b className="keyword">customer service</b>. My hobbies have taught me <b className="keyword">patience</b>
+                and <b className="keyword">perseverance</b>. Now I'm studying to be <b className="keyword">IT-support</b> staff at <a className="about-link" href="https://www.taitotalo.fi/" target="blank">Taitotalo</a>.
                 <br />
                 "I am the missing link between IT and the user."
                 
             </p>
             <br />
-            <div class='cvBtns'>
+            <div className='cvBtns'>
                 <p>
                     CV in PDF & Video:
                 </p>
                 <div>
                     <a href="./documents/cv_image.png" target="_blank">
-                        <button class='appButton'>
-                            <span class="material-symbols-outlined">visibility</span>
+                        <button className='appButton'>
+                            <span className="material-symbols-outlined">visibility</span>
                         </button>
                     </a>
                     <a href="./documents/CV_victor_grinan_2025_fi_en.pdf" target="_blank" download='CV_victor_grinan_2025_fi_en'>
-                        <button class='appButton'>
-                            <span class="material-symbols-outlined">download</span>
+                        <button className='appButton'>
+                            <span className="material-symbols-outlined">download</span>
                         </button>
                     </a>
                     <a href="https://www.youtube.com/watch?v=FXtfp2-87D8" target="_blank">
-                        <button class='appButton'>
-                            <span class="material-symbols-outlined">
+                        <button className='appButton'>
+                            <span className="material-symbols-outlined">
                                 play_circle
                             </span>
                         </button>
@@ -58,30 +58,30 @@ const About = () => {
                 </p>
                 <div>
                     <a href="https://www.hs.fi/kaupunki/art-2000008121681.html" target="_blank">
-                        <button class='appButton'>
-                            <span class="material-symbols-outlined">
+                        <button className='appButton'>
+                            <span className="material-symbols-outlined">
                                 newspaper
                             </span>
                         </button>
                     </a>
                     <a href="https://www.hs.fi/ulkomaat/art-2000008119341.html" target="_blank">
-                        <button class='appButton'>
-                            <span class="material-symbols-outlined">
+                        <button className='appButton'>
+                            <span className="material-symbols-outlined">
                                 newspaper
                             </span>
                         </button>
                     </a>
                     <a href="https://tutka.pro/yhteiskunta/totuus-turistiverhon-takana/" target="_blank">
-                        <button class='appButton'>
-                            <span class="material-symbols-outlined">
+                        <button className='appButton'>
+                            <span className="material-symbols-outlined">
                                 newspaper
                             </span>
                         </button>
                     </a>
                     <a href="https://librodeinsidencias.blogspot.com/" target="_blank">
-                        <button class='appButton'>
-                            <span class="material-symbols-outlined">
-                                <span class="material-symbols-outlined">
+                        <button className='appButton'>
+                            <span className="material-symbols-outlined">
+                                <span className="material-symbols-outlined">
                                     ink_pen
                                 </span>
                             </span>
@@ -92,21 +92,21 @@ const About = () => {
 
           
             <br />
-            <h2 class="title"> About this page </h2>
+            <h2 className="title"> About this page </h2>
 
-            <p class="about">
+            <p className="about">
                 This is just a static but mobile-friendly page displaying some of the small projects that I have created
                 as part of my web development learning journey. I started this portfolio when I was schooling and kept
-                adding features and small improvements ever since, without changing much the <a class="about-link"
+                adding features and small improvements ever since, without changing much the <a className="about-link"
                     href="./documents/web_designs.jpg" target="_blank">original design.</a> made as a class assignment
-                in the first weeks of <a class="about-link" href="https://en.bc.fi/qualifications/full-stack-web-developer-program/"
+                in the first weeks of <a className="about-link" href="https://en.bc.fi/qualifications/full-stack-web-developer-program/"
                     target="blank">HBC</a> 01/2022.
                 <br />
-                <a class="about-link" target="_blank" href="https://github.com/Victor-Grinan-Dev/Portfolio"> Source code in Github</a>
+                <a className="about-link" target="_blank" href="https://github.com/Victor-Grinan-Dev/Portfolio"> Source code in Github</a>
             </p>
 
         </article>
-       </div>
+
     </SectionWrapper>
   )
 }
@@ -114,48 +114,48 @@ const About = () => {
 export default About
 
 /**
-            <article id="aboutArticle" class="bgLevel1">
+            <article id="aboutArticle" className="bgLevel1">
                     
-            <h2 class="title"> About me </h2>
+            <h2 className="title"> About me </h2>
 
-            <p class="about">
+            <p className="about">
                 <br />
                 <br />
-                I am a computers <b class="keyword">enthusiast</b>.
+                I am a computers <b className="keyword">enthusiast</b>.
                 <br />
                 I started learning coding independently in 2019, and in June 2023 I graduated in Information and
-                communication technologies from <a class="about-link"
+                communication technologies from <a className="about-link"
                     href="https://en.bc.fi/qualifications/full-stack-web-developer-program/" target="blank">Helsinki
                     Business College (HBC)</a>, where I gained a solid understanding of programming, algorithms, and
                 software design principles.
                 <br />
                 My background is in pedagogy and team leading. This has taught me to see the value of
-                good <b class="keyword">group dynamics</b>. In my previous workplaces, I have quickly advanced to responsible
-                positions. I am particularly good at problem solving and <b class="keyword">customer service</b>. My hobbies have taught me <b class="keyword">patience</b>
-                and <b class="keyword">perseverance</b>. Now I'm studying to be <b class="keyword">IT-support</b> staff at <a class="about-link" href="https://www.taitotalo.fi/" target="blank">Taitotalo</a>.
+                good <b className="keyword">group dynamics</b>. In my previous workplaces, I have quickly advanced to responsible
+                positions. I am particularly good at problem solving and <b className="keyword">customer service</b>. My hobbies have taught me <b className="keyword">patience</b>
+                and <b className="keyword">perseverance</b>. Now I'm studying to be <b className="keyword">IT-support</b> staff at <a className="about-link" href="https://www.taitotalo.fi/" target="blank">Taitotalo</a>.
                 <br />
                 "I am the missing link between IT and the user."
                 
             </p>
             <br />
-            <div class='cvBtns'>
+            <div className='cvBtns'>
                 <p>
                     CV in PDF & Video:
                 </p>
                 <div>
                     <a href="./documents/cv_image.png" target="_blank">
-                        <button class='appButton'>
-                            <span class="material-symbols-outlined">visibility</span>
+                        <button className='appButton'>
+                            <span className="material-symbols-outlined">visibility</span>
                         </button>
                     </a>
                     <a href="./documents/CV_victor_grinan_2025_fi_en.pdf" target="_blank" download='CV_victor_grinan_2025_fi_en'>
-                        <button class='appButton'>
-                            <span class="material-symbols-outlined">download</span>
+                        <button className='appButton'>
+                            <span className="material-symbols-outlined">download</span>
                         </button>
                     </a>
                     <a href="https://www.youtube.com/watch?v=FXtfp2-87D8" target="_blank">
-                        <button class='appButton'>
-                            <span class="material-symbols-outlined">
+                        <button className='appButton'>
+                            <span className="material-symbols-outlined">
                                 play_circle
                             </span>
                         </button>
@@ -168,30 +168,30 @@ export default About
                 </p>
                 <div>
                     <a href="https://www.hs.fi/kaupunki/art-2000008121681.html" target="_blank">
-                        <button class='appButton'>
-                            <span class="material-symbols-outlined">
+                        <button className='appButton'>
+                            <span className="material-symbols-outlined">
                                 newspaper
                             </span>
                         </button>
                     </a>
                     <a href="https://www.hs.fi/ulkomaat/art-2000008119341.html" target="_blank">
-                        <button class='appButton'>
-                            <span class="material-symbols-outlined">
+                        <button className='appButton'>
+                            <span className="material-symbols-outlined">
                                 newspaper
                             </span>
                         </button>
                     </a>
                     <a href="https://tutka.pro/yhteiskunta/totuus-turistiverhon-takana/" target="_blank">
-                        <button class='appButton'>
-                            <span class="material-symbols-outlined">
+                        <button className='appButton'>
+                            <span className="material-symbols-outlined">
                                 newspaper
                             </span>
                         </button>
                     </a>
                     <a href="https://librodeinsidencias.blogspot.com/" target="_blank">
-                        <button class='appButton'>
-                            <span class="material-symbols-outlined">
-                                <span class="material-symbols-outlined">
+                        <button className='appButton'>
+                            <span className="material-symbols-outlined">
+                                <span className="material-symbols-outlined">
                                     ink_pen
                                 </span>
                             </span>
@@ -202,17 +202,17 @@ export default About
 
           
             <br />
-            <h2 class="title"> About this page </h2>
+            <h2 className="title"> About this page </h2>
 
-            <p class="about">
+            <p className="about">
                 This is just a static but mobile-friendly page displaying some of the small projects that I have created
                 as part of my web development learning journey. I started this portfolio when I was schooling and kept
-                adding features and small improvements ever since, without changing much the <a class="about-link"
+                adding features and small improvements ever since, without changing much the <a className="about-link"
                     href="./documents/web_designs.jpg" target="_blank">original design.</a> made as a class assignment
-                in the first weeks of <a class="about-link" href="https://en.bc.fi/qualifications/full-stack-web-developer-program/"
+                in the first weeks of <a className="about-link" href="https://en.bc.fi/qualifications/full-stack-web-developer-program/"
                     target="blank">HBC</a> 01/2022.
                 <br />
-                <a class="about-link" target="_blank" href="https://github.com/Victor-Grinan-Dev/Portfolio"> Source code in Github</a>
+                <a className="about-link" target="_blank" href="https://github.com/Victor-Grinan-Dev/Portfolio"> Source code in Github</a>
             </p>
 
         </article>
--- src/components/home/sections/contact/Contact.jsx
+++ src/components/home/sections/contact/Contact.jsx
@@ -1,11 +1,66 @@
 import SectionWrapper from "../../../UIs/sectionWrapper/SectionWrapper"
+// import email from "icons/email_color.png"
+// import gitlab from "./icons/gitLab.png"
+// import github from "./icons/github_circle_white_border.png"
+// import Linkedin from "./icons/Linkedin-logo-transparent-PNG.png"
+// import Instagram from "./icons/instagram_color.png"
+// import Twitter from "./icons/Twitter-X.png"
+// import youtube from "./icons/youtube.png"
 
 const Contact = () => {
   return (
-    <SectionWrapper>
-        <div>
-            Contact
+    <SectionWrapper id='footer' className="bgLevel5">
+   
+
+        <h4 id='contact' className="section-title">Conctact me:</h4>
+
+        {/* <div className="email">
+
+            <a href="https://mail.google.com/mail/u/0/#inbox" target="blank">
+                <img src="icons/email_color.png" alt="email" id="email" class="icon">
+            </a>
+
+            <a href="https://mail.google.com/mail/u/0/#inbox" target="blank">
+                <p id="emailActual"> victorgrinan@gmail.com </p>
+            </a>
         </div>
+
+        <div id="some">
+            <a href="https://gitlab.com/VictorGrinan" target="blank">
+                <img src={gitlab} alt="GitLab">
+            </a>
+            <a href="https://github.com/Victor-Grinan-Dev" target="blank">
+                <img src={github} alt="GitHub">            
+            </a>
+
+            <a href="https://www.linkedin.com/in/victor-gri%C3%B1an-892a741b7/" target="blank">
+                <img src={Linkedin}>
+            </a>
+
+            <a href="https://www.instagram.com/victor_grinan_dev/" target="blank">
+                <img src={Instagram}>
+            </a>
+
+            <a href="https://twitter.com/GrinanVictor" target="blank">
+                <img src={Twitter} alt="Twitter">
+            </a>
+            <a href='https://www.youtube.com/'>
+                <img src={youtube}>
+            </a>
+        </div> */}
+
+        {/* <p>This page <a href="https://github.com/Victor-Grinan-Dev/Portfolio">source code</a> is accesible from my
+            Github account.</p> */}
+        <section id="licenseSection">
+            <p id="copyRight" className="copyRight">Copyrights &copy; <span className="year"></span></p>
+            <p id="love">Made with ❤️ from Finland</p>
+        </section>
+
+        <button id="scrollUp appButton" className="scrollUp invisible">
+          <span className="material-symbols-outlined">
+                arrow_upward
+            </span></button>
+
     </SectionWrapper>
   )
 }
